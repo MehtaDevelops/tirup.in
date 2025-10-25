6801f71a62d5c205e8924a882b986aa8deff4477
quantumbegins <quantumvanya@gmail.com>
2025-10-25 16:45:35 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import Script from "next/script"
 import "./globals.css"
 import PageTransition from "@/components/page-transition"
 
@@ -14,7 +15,7 @@ export const metadata: Metadata = {
   title: "Tirup Mehta | Designer & Developer",
   description:
     "Portfolio of Tirup Mehta, a front-end web developer, cybersecurity enthusiast, and UI/UX designer from Gujarat, India.",
-    generator: 'v0.app'
+  generator: "tirup.in",
 }
 
 export default function RootLayout({
@@ -24,7 +25,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N37XXZ2B');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
+
       <body className={`${inter.variable} font-sans bg-white text-black antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N37XXZ2B"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <PageTransition>{children}</PageTransition>
       </body>
     </html>
