97960367e77904f9a1bab9e62f2c263a22e49a7d
quantumbegins <quantumvanya@gmail.com>
2025-10-26 18:14:13 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,14 @@ export default function RootLayout({
         </Script>
         {/* End Google Tag Manager */}
 
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "tvo11b3to0");
+</script>
+        
         {/* Google Analytics (gtag.js) */}
         <Script
           async
