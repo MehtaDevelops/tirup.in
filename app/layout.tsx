b9e7b4b18c37e3bdecb419c9ec02b672292b4bc7
MehtaDevelops <quantumvanya@gmail.com>
2026-06-27 18:10:01 +0530

    security: harden Content Security Policy by hashing inline scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,11 +89,7 @@ export default function RootLayout({
 
         {/* Google Tag Manager */}
         <Script id="google-tag-manager" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-N37XXZ2B');`}
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N37XXZ2B');`}
         </Script>
         {/* End Google Tag Manager */}
 
@@ -104,10 +100,7 @@ export default function RootLayout({
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
-          {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-BKS7VBCPH5');`}
+          {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-BKS7VBCPH5');`}
         </Script>
         {/* End Google Analytics */}
       </head>
